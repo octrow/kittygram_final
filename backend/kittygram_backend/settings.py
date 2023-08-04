b01822490a5b0edb0e1b59f2ebbe0d14f2fccb63
Daniil Petrov <octrow@yandex.ru>
2023-08-04 09:32:19 +0600

    v0.20

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,6 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
+
 DEBUG = False
 
 ALLOWED_HOSTS = ['51.250.107.180', '127.0.0.1', 'localhost', 'kittigram.mooo.com']
